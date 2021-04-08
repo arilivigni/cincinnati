4e38fafb94121ba295d32df7c620a7426a9920b5
Ari LiVigni <arilivigni@gmail.com>
2021-04-07 21:08:17 -0400

    updated with storage change to buildah

--- dist/app_sre_buildah_deploy.sh
+++ dist/app_sre_buildah_deploy.sh
@@ -9,23 +9,27 @@ DOCKERFILE_BUILD="$DOCKERFILE_CONTEXT_DIR/Dockerfile.build"
 DOCKERFILE_DEPLOY="$DOCKERFILE_CONTEXT_DIR/Dockerfile.deploy"
 
 # build cincinnati builder image to use as base for the deployment image
-buildah bud --tls-verify=$SSL_VERIFY \
+buildah bud --storage-driver=vfs \
+--tls-verify=$SSL_VERIFY \
 --layers \
 -f $DOCKERFILE_BUILD/Dockerfile \
 -t $QUAY_IMG:builder $DOCKERFILE_BUILD
 
 # build cincinnati deploy image
-buildah bud --tls-verify=$SSL_VERIFY \
+buildah bud --storage-driver=vfs \
+--tls-verify=$SSL_VERIFY \
 --layers \
 -f $DOCKERFILE_BUILD/Dockerfile \
 -t $QUAY_IMG:$GIT_HASH $DOCKERFILE_DEPLOY
 
 # deploy image with to quay.io with git hash to image tag
-buildah push --tls-verify=$SSL_VERIFY \
+buildah push --storage-driver=vfs \
+--tls-verify=$SSL_VERIFY \
 $QUAY_IMG:$GIT_HASH \
 $QUAY_IMG:$GIT_HASH
 
 # deploy image to quay.io with latest tag
-buildah push --tls-verify=$SSL_VERIFY \
+buildah push --storage-driver=vfs \
+--tls-verify=$SSL_VERIFY \
 $QUAY_IMG:$GIT_HASH \
 $QUAY_IMG:latest
